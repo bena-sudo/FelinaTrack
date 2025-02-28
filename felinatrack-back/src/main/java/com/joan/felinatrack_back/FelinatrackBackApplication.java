44704ce90e526b3dd07700b68ea4432beff81355
bena-sudo <joanbenaventcastells@gmail.com>
2025-03-01 00:29:50 +0100

    create back project

---
+++ felinatrack-back/src/main/java/com/joan/felinatrack_back/FelinatrackBackApplication.java
@@ -0,0 +1,13 @@
+package com.joan.felinatrack_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FelinatrackBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FelinatrackBackApplication.class, args);
+	}
+
+}
